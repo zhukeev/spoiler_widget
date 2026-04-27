a03ad1cd890988b9999726501c9a92d4df1fb63f
Argen Zhukeev <djukeevar@gmail.com>
2026-04-27 18:27:24 +0600

    chore: version bump

--- lib/models/spoiler_drawing_strategy.dart
+++ lib/models/spoiler_drawing_strategy.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-import 'dart:typed_data';
+
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
--- lib/utils/image_factory.dart
+++ lib/utils/image_factory.dart
@@ -1,7 +1,6 @@
 import 'dart:math' as math;
 import 'dart:ui' as ui;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 @immutable
